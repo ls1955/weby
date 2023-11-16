4d5fd18f09a550da72ea6baae69874d8f0614835
ls1955 <xxlsxx1955@gmail.com>
2023-11-16 14:24:16 +0800

    Use erb

--- app.rb
+++ app.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: false
 
 require "debug"
+require "erb"
 
 # :nodoc:
 class App
   def call(_)
     headers = { "Content-type" => "text/html" }
 
-    response = File.read("app/views/index.html")
+    main_header = "Goodbye, world."
+    paragraph = "Yet another day."
+
+    erb = ERB.new(html_template)
+    response = erb.result(binding)
+
     [200, headers, [response]]
+  end
+
+  def html_template
+    File.read("app/views/index.html.erb")
   end
 end
