d0c4d382528502cc6fb80b0945c774461c60c79c
ls1955 <xxlsxx1955@gmail.com>
2023-11-16 14:14:17 +0800

    Serve static files via Rack::Static

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: false
 
+require "debug"
+
 # :nodoc:
 class App
   def call(_)
     headers = { "Content-type" => "text/html" }
-    response = File.read("#{__dir__}/app/views/index.html")
 
+    response = File.read("app/views/index.html")
     [200, headers, [response]]
   end
 end
--- config.ru
+++ config.ru
@@ -4,5 +4,7 @@ require "rack"
 require_relative "app"
 
 use Rack::Reloader, 0
+use Rack::Static, urls: %w[/public /favicon.ico]
+
 # More or less App.new.call(env) underneath...
 run App.new
