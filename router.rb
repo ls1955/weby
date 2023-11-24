234f3ba4276263f1807c0c2452a1c012e6e1b0c8
ls1955 <xxlsxx1955@gmail.com>
2023-11-24 21:24:03 +0800

    Modify #get
    Now if path in the form of "controller#action" is given,
    it will append a block that return the result of render
    view instead.

--- router.rb
+++ router.rb
@@ -19,19 +19,23 @@ class Router
     @routes = {}
   end
 
-  # Appends the blk into the routes if blk is given
-  # If the path is in form of "controller#action", append controller's action into the routes
+  # Appends the blk into the routes and return if blk is given
+  # If path in form "controller#action", append the block that return rendered view
   def get(path, &blk)
     return @routes[path] = blk if blk
 
     return unless path.include? "#"
 
-    ctrller, action = path.split "#"
-    ctrller_klass = Object.const_get("#{ctrller.capitalize}Controller")
+    ctrller_name, action = path.split "#"
+    ctrller_klass = Object.const_get("#{ctrller_name.capitalize}Controller")
     # For now turn "controller#action" to "/controller/action" path
     new_path = "/#{path.tr('#', '/')}"
 
-    @routes[new_path] = ->(env) { ctrller_klass.new(env).send(action) }
+    @routes[new_path] = proc do |env|
+      ctrller = ctrller_klass.new env
+      ctrller.send action
+      ctrller.render "app/views/#{ctrller_name}/#{action}.html.erb"
+    end
   end
 
   # Calls the handler for the route
