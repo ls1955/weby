59c536eac0ef6601d3d8cb36bd7964fa00807e7d
ls1955 <xxlsxx1955@gmail.com>
2023-11-24 21:21:16 +0800

    Include #render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: false
 
+require "erb"
+
 # :nodoc:
 class ApplicationController
   def initialize(env)
     @env = env
+  end
+
+  def render(view_path)
+    erb_template = ERB.new File.read(view_path)
+    erb_template.result binding
   end
 end
