4af42ec6df6912923eeb4b0c95577ef7edc38a7c
ls1955 <xxlsxx1955@gmail.com>
2023-11-24 21:07:19 +0800

    Include some comments
    For future self.

--- router.rb
+++ router.rb
@@ -8,6 +8,7 @@ require_relative "app/controllers/articles_controller"
 class Router
   include Singleton
 
+  # A wrapper to instance_eval the block
   class << self
     def draw(&)
       instance.instance_eval(&)
@@ -18,6 +19,8 @@ class Router
     @routes = {}
   end
 
+  # Appends the blk into the routes if blk is given
+  # If the path is in form of "controller#action", append controller's action into the routes
   def get(path, &blk)
     return @routes[path] = blk if blk
 
@@ -25,11 +28,13 @@ class Router
 
     ctrller, action = path.split "#"
     ctrller_klass = Object.const_get("#{ctrller.capitalize}Controller")
+    # For now turn "controller#action" to "/controller/action" path
     new_path = "/#{path.tr('#', '/')}"
 
     @routes[new_path] = ->(env) { ctrller_klass.new(env).send(action) }
   end
 
+  # Calls the handler for the route
   def build_response(env)
     path = env["REQUEST_PATH"]
     handler = @routes.fetch(path) { proc { "No route for #{path}" } }
